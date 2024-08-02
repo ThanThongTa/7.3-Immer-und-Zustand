cd7894c791b1b376dcf117e5e86c27e4bdbdce5e
Thong Ta <thanthong.ta@gmail.com>
2024-08-02 06:19:06 +0200

    testing framer motion

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Shop from './components/ImmerShop';
+import { motion } from 'framer-motion';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+		<motion.div animate={{ x: 100 }}>Test</motion.div>
 		<Shop />
 	</React.StrictMode>
 );
